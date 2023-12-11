8979c4f366913c939745aa749659b1ac6dcaa139
oceanoftears <129828478+OceanOfTears@users.noreply.github.com>
2023-12-11 14:07:03 +0700

    Add files via upload

---
+++ pascal_triangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Triangle
+{
+    static void Main()
+    {
+        int num;
+        Console.Write("Размер матрицы x*x,введите x: ");
+        num = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[num, num];
+        int[,] way = new int[num, num];
+
+        for (int i = 0; i < num; i++)
+        {
+            way[i, 0] = 1;
+        }
+        for (int j = 0; j < num; j++)
+        {
+            way[0, j] = 1;
+        }
+
+        for (int i = 1; i < num; i++)
+        {
+            for (int j = 1; j < num; j++)
+            {
+                way[i, j] = way[i - 1, j] + way[i, j - 1];
+            }
+        }
+        Console.WriteLine($"Количество путей в матрице {num}*{num}: {way[num - 1, num - 1]}");
+    }
+}
+
